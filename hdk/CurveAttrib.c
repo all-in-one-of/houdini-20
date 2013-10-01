b4f1e2d6b2ce035125cee6a2a20151fd10fb7058
yongbin <yongbin@sideway>
2013-10-01 14:28:56 +0900

    delete some legacy code

--- hdk/CurveAttrib.c
+++ hdk/CurveAttrib.c
@@ -1,32 +1,4 @@
-/*
- * Copyright (c) 2013
- *  Side Effects Software Inc.  All rights reserved.
- *
- * Redistribution and use of Houdini Development Kit samples in source and
- * binary forms, with or without modification, are permitted provided that the
- * following conditions are met:
- * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- * 2. The name of Side Effects Software may not be used to endorse or
- *    promote products derived from this software without specific prior
- *    written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY SIDE EFFECTS SOFTWARE `AS IS' AND ANY EXPRESS
- * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN
- * NO EVENT SHALL SIDE EFFECTS SOFTWARE BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
- * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- *----------------------------------------------------------------------------
- */
-
- /// It's my first hdk node. not really first... anyway.
- /// calculate curve's tangent or N
+// calculate curve's tangent or N
 
 #include <UT/UT_DSOVersion.h>
 #include <SYS/SYS_Math.h>
@@ -39,9 +11,6 @@
 #include <OP/OP_OperatorTable.h>
 #include <GA/GA_AIFTuple.h>
 #include <UT/UT_Vector3.h>
-// #include <UT/UT_COLOR.h>
-
-// #include <GA/GA_AttributeRef.h>
 #include "CurveAttrib.h"
 
 using namespace HDK_Sample;
@@ -89,10 +58,6 @@ CurveAttrib::cookMySop(OP_Context &context)
 
     // Duplicate input geometry
     duplicateSource(0, context);
-
-    // Flag the SOP as being time dependent (i.e. cook on time changes)
-    // I will use it when it's input nodes change time dependently
-    // flags().timeDep = 1;
 
     // fpreal frame = OPgetDirector()->getChannelManager()->getSample(context.getTime());
 
